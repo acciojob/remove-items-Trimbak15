899fe8baa61a0de09883036fcc60b9670537e366
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-05-01 18:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const colorSelect = document.getElementById('colorSelect');
+const removeColor = document.getElementById('removeColorButton');
+
+removeColor.addEventListener("click", function() {
+    const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+    const selectColor = selectedOption.value;
+
+    // remove the end child 
+    colorSelect.removeChild(selectedOption);
+
+});
